f00e05796f394dab4df8d8ea3f951e2c16b213be
Arata Yano <aratayano00@gmail.com>
2020-01-12 02:56:13 +0200

    almost done with this

--- swingy/src/main/java/Swingy/App.java
+++ swingy/src/main/java/Swingy/App.java
@@ -14,6 +14,12 @@ public class App {
             Output display = new Output();
             display.Begin();
             String choice = Reader.readLine();
+            while (!choice.equals("1") && !choice.equals("2")) {
+                System.out.println("Your choices are:\n" +
+                        "1 - Start a new game\n" +
+                        "2 - Load a game\n");
+                choice = Reader.readLine();
+            }
             gamecontroller.Game(Integer.parseInt(choice));
         } catch (IOException e) {
             System.out.println("Error reading input!");
