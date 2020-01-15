40dfd1c7e9eb8507abb653e6ba3fe587e9f2a5e5
Arata Yano <aratayano00@gmail.com>
2020-01-15 13:46:29 +0200

    asdasd

--- swingy/src/main/java/Swingy/controller/GameData.java
+++ swingy/src/main/java/Swingy/controller/GameData.java
@@ -4,13 +4,19 @@ import view.*;
 import java.io.*;
 
 public class GameData {
-    public static PrintWriter printWriter = null;
-
+    private PrintWriter printWriter = new PrintWriter(new File("./saves.txt"));
     public GameData() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader ("../saves.txt"));
     };
 
-    public SaveGame(Hero player, String[][] map) {
-
+    public void SaveGame(Hero player, String[][] map) throws IOException {
+        String line;
+        BufferedReader reader = new BufferedReader(new FileReader ("../saves.txt"));
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            if (player.Name == line) {
+                // do shit
+            }
+        }
     }
 }
